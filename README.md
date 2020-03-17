a867025561d96d91f898eb19689625c4f18d3104
1584480413@qq.com <1584480413@qq.com>
2020-03-17 18:03:37 +0800

    添加vue版本

--- README.md
+++ README.md
@@ -1,35 +1,34 @@
-标题     drag-guide
-辅助说明 a drag-guide component 一个基于vue开发的可拖拽宫格插件，兼容多端.
-辅助说明         拖拽改变方式按照安卓原生模块改变的 进行开发.
+###    drag-guide
+###  a drag-guide component 一个基于vue开发的可拖拽宫格插件，兼容多端.
+<p>拖拽改变方式按照安卓原生模块改变的样式 进行开发.</p>         
 
+<br>
+##### h5 demo版本可直接使用live-serve进行预览
+<ul>
+<li>安装 npm install -g live-server</li>
+<li>使用  在命令行中输入 live-server</li>
+<li>如果同时启动多个项目，为防止冲突，可以分配不同的端口号</li>
+<li> live-server --port=8282</li>
+<li>live-server --port=8585</li>
+</ul>
 
+<br>
 
-一、h5 demo版本可直接使用live-serve进行预览
-1.安装 npm install -g live-server
-2.使用  在命令行中输入 live-server
-3.如果同时启动多个项目，为防止冲突，可以分配不同的端口号
-4. live-server --port=8282
-5. live-server --port=8585
+##### 后续会提供图片可进行查看.
+<br>
+#####  默认提供vue版本 以及 uni-app版本 经尝试可以适应于各端，Android不兼容4.4以下版本.如有问题请联系qq.
 
+<br>
 
+##### 默认使用说明：
 
-二、后续会提供图片可进行查看.
-
-三、 默认提供vue版本 以及 uni-app版本 经尝试可以适应于各端，Android不兼容4.4以下版本.如有问题请联系qq.
-
-
-
-四、 使用说明：
-
-
-辅助说明 引入后只需要在插件页修改 具体要使用的宫格数row，默认为九宫格
-辅助说明 row的数值 实现不同的 宫格
-
-
-辅助说明 addModule 按自身需求引入.里面可直接使用.无需任何参数
-
-辅助说明 图片资源可自行在static/all            默认的模块图片
-辅助说明               static/all/extra       额外的模块图片
-
-
-辅助说明 imgHeight 以及 imgWidth 定义宫格内每一个图片的大小,请记得计算row宫格数，避免图片大小超出造成样式崩坏
+<br>
+<ul>
+<li>引入后只需要在插件页修改 具体要使用的宫格数row，默认为九宫格
+<li> row的数值 实现不同的 宫格
+<li> imgHeight 以及 imgWidth 定义宫格内每一个图片的大小,请记得计算row宫格数，避免图片大小超出造成样式崩坏
+<li> addModule 按自身需求引入.里面可直接使用.无需任何参数
+<br>
+<li> 图片资源可自行在static/all            默认的模块图片
+<li>               static/all/extra       额外的模块图片
+</ul>
---
+++ vue/components/addModule/addModule.vue
@@ -0,0 +1,332 @@
+<template>
+	<view class="body">
+		<view class="page-bd">
+		<view class="module-box" v-for="(item, m) in module" :key="m">
+			<text>{{item.name}}</text>
+			<view :class="{special:d!=(item.detail.length-1),module:true}" v-for="(detail,d) in item.detail" :key="d">
+					<image  :src="moduleImg(detail.img)" mode="widthFix" />
+					<view >
+						<text>{{detail.title}}</text>
+						<text>{{detail.detail}}</text>
+					</view>
+					<view class="select-box" @tap="select(detail.selected,m,d)">
+						<image src="../../static/selected_03.png" mode="widthFix"  v-show="detail.selected"/>
+						<view v-show="!detail.selected"/>
+					</view>
+			</view>
+		</view>
+		</view>
+		<view class="btn-box">
+			<button type="button"  @tap="addModule">添加</button>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name:'addModule',
+	data() {
+		return {
+			matchArray:[],
+			module: [
+				{
+					name: '我的',
+					detail: [
+						{
+							title: '我的订单',
+							detail: '我向上级提交的全部订单',
+							img: 'goods',
+							selected:false
+						},
+						{
+							title: '我要进货',
+							detail: '我向上级代理商进货的入口',
+							img: 'mycart',
+							selected:false
+						},
+						{
+							title: '邀请代理',
+							detail: '向下级代理发出邀请',
+							img: 'invite',
+							selected:false
+						},
+						{
+							title: '充值货款',
+							detail: '向下级代理发出邀请',
+							img: 'pay',
+							selected:false
+						},
+						{
+							title: '我要提货',
+							detail: '向下级代理发出邀请',
+							img: 'extra/withdraw' ,
+							selected:false
+						},
+						{
+							title: '我的库存',
+							detail: '向下级代理发出邀请',
+							img: 'store',
+							selected:false
+						},
+						{
+							title: '升级记录',
+							detail: '向下级代理发出邀请',
+							img:  'levelupdetail',
+							selected:false
+						},
+						{
+							title: '申请升级',
+							detail: '向下级代理发出邀请',
+							img: 'levelup',
+							selected:false
+						},
+						{
+							title: '我的授权',
+							detail: '向下级代理发出邀请',
+							img: 'extra/permission',
+							selected:false
+						},
+						{
+							title: '我的钱包',
+							detail: '向下级代理发出邀请',
+							img: 'wallet',
+							selected:false
+						},
+						{
+							title: '我的业绩',
+							detail: '向下级代理发出邀请',
+							img: 'record',
+							selected:false
+						}
+					]
+				},
+				{
+					name: '团队',
+					detail: [
+						{
+							title: '我的上级',
+							detail: '我向上级提交的全部订单',
+							img:  'extra/parent' ,
+							selected:false
+						},
+						{
+							title: '下级订单',
+							detail: '我向上级提交的全部订单',
+							img:  'extra/childcart',
+							selected:false
+						},
+						{
+							title: '我的团队',
+							detail: '我向上级提交的全部订单',
+							img: 'extra/myterm' ,
+							selected:false
+						},
+						{
+							title: '新代理审核',
+							detail: '我向上级提交的全部订单',
+							img:  'extra/invitereview'  ,
+							selected:false
+						},
+						{
+							title: '升级审核',
+							detail: '我向上级提交的全部订单',
+							img: 'extra/levelupreview'  ,
+							selected:false
+						},
+						{
+							title: '充值审核',
+							detail: '我向上级提交的全部订单',
+							img: 'extra/payreview',
+							selected:false
+						}
+					]
+				},
+				{               
+					name: '其他',
+					detail: [
+						{
+							title: '素材中心',
+							detail: '我向上级提交的全部订单',
+							img: 'extra/source',
+							selected:false
+						},
+						{
+							title: '维权举报',
+							detail: '我向上级提交的全部订单',
+							img: 'extra/safeguard',
+							selected:false
+						},
+						{
+							title: '授权查询',
+							detail: '我向上级提交的全部订单',
+							img:  'extra/permissionsearch',
+							selected:false
+						},
+						{
+							title: '意见反馈',
+							detail: '我向上级提交的全部订单',
+							img: 'extra/advice',
+							selected:false
+						},
+						{
+							title: '积分商城',
+							detail: '我向上级提交的全部订单',
+							img:'extra/pointsmall',
+							selected:false
+						}
+					]
+				}
+			]
+		};
+	},
+	created() {
+		//按需使用
+		//获取vuex里 image 并根据匹配的模块 添加选中的样式 
+			// success:(res)=>{
+			// 	this.matchArray=res.data
+			// 	for(const m in this.matchArray){
+			// 		this.module.forEach((value,index)=>{
+			// 			for(const d of value.detail){
+			// 				if(d.img===this.matchArray[m].img){
+			// 					d.selected=true  
+			// 				}
+			// 			}
+			// 		})
+			// 	}
+			// }
+	},
+	methods: {
+		moduleImg(path) {
+			return '../../static/all/' + path + '.png';
+		},
+		select(state,m,d){
+			this.module[m].detail[d].selected=!this.module[m].detail[d].selected
+		},
+		addModule(){
+			// 按需使用 把此时所有all的数据放入vuex 替换原image  并跳转回 拖拽模块页 路由
+			// const all=[]
+			// this.module.forEach( value=>{
+			// 	for(const d of value.detail){
+			// 		if(d.selected){
+			// 			const obj={
+			// 				img:d.img,
+			// 			}
+			// 			all.push(obj)
+			// 		}
+			// 	}
+			// })
+			// this.$route.push()
+		}
+	}
+	
+};
+</script>
+
+<style>
+.body{
+	background:rgba(255,255,255,1);
+	font-family: PingFang SC;
+	overflow:auto;
+}
+
+.page-bd{
+	padding-bottom:50px;
+}
+
+.module-box{
+	width: 100%;
+}
+
+.module-box>text{
+	width:100%;
+	height: 30px;
+	line-height: 30px;
+	background:#F7F7F7;
+	box-sizing: border-box;
+	padding-left:17px;
+	display: block;
+	font-size:12px;
+	font-weight:500;
+	color:rgba(74,74,74,1);
+}
+
+.module{
+	widhth:100%;
+	display:flex;
+	align-content:center;
+	box-sizing: border-box;
+	background-color: #fff;
+	padding: 8px 17px 0 17px;
+	position: relative;
+}
+
+.special{
+	border-bottom: 1px solid #EDEDED;
+}
+
+.module>image{
+	margin-right:16.5px;
+	width:57px;
+	height:57px;
+	background:rgba(245,247,249,1);
+	border-radius:50%;
+}
+
+.select-box{
+	width: 50px;
+	height:50px;
+	position: absolute;
+	right: 12.5px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.select-box>view, .select-box>image{
+	width:18px;
+	height:18px;
+	border-radius:50%;
+	z-index: 2;
+}
+
+.select-box>view{
+	border: 1px solid rgba(141,141,141,1);
+}
+
+.module>view text{
+	font-size:15px;
+	font-weight:bold;
+	color:rgba(34,34,34,1);
+	display: block;
+	margin-top:11.5px;
+}
+
+.module>view text+text{
+	font-size:12px;
+	font-weight:500;
+	color:rgba(141,141,141,1);
+	margin-top:6.5px;
+}
+
+.btn-box{
+	width: 100%;
+	box-sizing: border-box;
+	position:fixed;
+	bottom: 0;
+	z-index: 2;
+	padding: 7px  25px;
+	background-color: #fff;
+}
+
+.btn-box>button{
+	line-height: 35px;
+	padding: 0;
+	height:35px;
+	width: 325px;
+	background:rgba(83,146,243,1);
+	border-radius:17.5px;
+	color:rgba(255,255,255,1);
+	font-size: 16px;
+	align-self: center;
+}
+</style>
